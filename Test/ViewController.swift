47e555eb84c6def69a34c11fee1070e545dab84b
krauserhuang <crescentdence@gmail.com>
2021-11-14 15:27:31 +0800

    Add label to storyboard which shows reversed text

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var reverseLabel: UILabel!
+    
+    let message = "Hello Git!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(reverse(text: "stressed"))
+        print(message)
+        
+        let reversed = reverse(text: "stressed")
+        print(reversed)
+        
+        reverseLabel.text = reversed
     }
     
     func reverse(text: String) -> String {
